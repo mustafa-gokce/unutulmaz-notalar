8b762a60f8886d21def534c533e7ef2fe266be7a
m <mustafagokce009@gmail.com>
2021-10-24 17:08:44 +0300

    worked on api

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ import flask
 # application settings
 name_application = "unutulmaz_notalar"
 application = flask.Flask(name_application,
-                          static_folder=os.path.abspath("/home/m/unutulmaz-notalar/static/"),
-                          static_url_path="/download_file/")
+                          static_folder=os.path.abspath("unutulmaz-notalar/static/"),
+                          static_url_path="/static/")
 
 
 # api get composers list
@@ -68,7 +68,7 @@ def api_get_songs(song_id):
 # prevent data leakage
 @application.errorhandler(404)
 def page_not_found(error):
-    return "restricted", 404
+    return "yasak", 404
 
 
 # api will run on port 80 with root privileges, else 8080 port
@@ -78,13 +78,13 @@ if __name__ == "__main__":
     try:
 
         # start the application on port 80, needs root privileges on linux
-        application.logger.info("Frontend Server Manager starting using Werkzeug WSGI on port 80...")
         application.run(debug=True, host="0.0.0.0", port=80, threaded=True)
+        application.logger.info("Frontend Server Manager started using Werkzeug WSGI on port 80!")
 
     # permission error handling
     except PermissionError:
 
         # fall back to port 8080 if root privileges cannot be obtained
-        application.logger.error("Cannot start Frontend Server Manager on port 80 because of the user privileges")
-        application.logger.info("Frontend Server Manager starting using Werkzeug WSGI on port 8080...")
+        application.logger.error("Cannot start Frontend Server Manager on port 80 because of the user privileges!")
         application.run(debug=True, host="0.0.0.0", port=8080, threaded=True)
+        application.logger.info("Frontend Server Manager started using Werkzeug WSGI on port 8080!")
