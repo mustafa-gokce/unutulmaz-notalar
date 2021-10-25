91c0aa39c56f28e00b8fd4f8262693490a961f67
m <mustafagokce009@gmail.com>
2021-10-25 14:26:06 +0300

    worked on data entry scripts

--- data_entry/png_to_pdf.py
+++ data_entry/png_to_pdf.py
@@ -1,23 +1,37 @@
 import os.path
+import PIL.Image
 
 folder_old = "/home/m/Notalar/"
 folder_new = "/home/m/pdfs/"
 
 i = 1
 while i < 15851:
-    if os.path.exists("{0}{1}.png".format(folder_old, i)):
-        print("Found:", i)
-    elif os.path.exists("{0}{1}_1.png".format(folder_old, i)):
-        j = 1
-        pages = []
-        while True:
-            if os.path.exists("{0}{1}_{2}.png".format(folder_old, i, j)):
-                pages.append(j)
-            else:
-                break
-            j += 1
-        print("Found:", i, pages)
+
+    if os.path.exists("{0}{1}.pdf".format(folder_new, i)):
+        print("Already converted:", i)
     else:
-        _ = 0
-        print("Not found:", i)
+
+        if os.path.exists("{0}{1}.png".format(folder_old, i)):
+            print("Found:", i)
+            image = PIL.Image.open("{0}{1}.png".format(folder_old, i))
+            image = image.convert("RGB")
+            image.save("{0}{1}.pdf".format(folder_new, i))
+        elif os.path.exists("{0}{1}_1.png".format(folder_old, i)):
+            j = 1
+            pages = []
+            images = []
+            while True:
+                if os.path.exists("{0}{1}_{2}.png".format(folder_old, i, j)):
+                    pages.append(j)
+                    image = PIL.Image.open("{0}{1}_{2}.png".format(folder_old, i, j))
+                    image = image.convert("RGB")
+                    images.append(image)
+                else:
+                    break
+                j += 1
+            print("Found:", i, pages)
+            images[0].save("{0}{1}.pdf".format(folder_new, i), save_all=True, append_images=images[1:])
+        else:
+            print("Not found:", i)
+
     i += 1
